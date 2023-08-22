fb420eab63a79d2a342e769aaaa77b78f2b02565
HafsiAhmed1920 <126480771+HafsiAhmed1920@users.noreply.github.com>
2023-08-22 17:54:14 +0200

    Add files via upload

--- main/main.py
+++ main/main.py
@@ -1,6 +1,6 @@
 from Common.reader import parquet_reader 
-from Common.saver import parquet_saver 
-from process.process import left_join 
+from Common.saver import save_parquet 
+from process.process import create_dataset_join1 , create_dataset_join2 , create_dataset_join3
 #main 
 maag_repa = parquet_reader(AppConfig.maag_repa_path)
 maag_master = parquet_reader(AppConfig.maag_master_path)
@@ -10,9 +10,9 @@ maag_raty = parquet_reader(AppConfig.maag_raty_path)
 
 
 
-leftjoin1 = left_join(maag_master,reac_ref,['c_act_type', 'n_applic_infq']) 
-leftjoin2 = left_join(leftjoin1 , maag_repa , ['N_APPLIC_INFQ' ,'C_MAST_AGREM_REFER'])
-leftjoin3 = left_join(leftjoin2 , maag_raty , 'c_mast_agrem_refer')
+leftjoin1 = create_dataset_join1(maag_master,reac_ref,['c_act_type', 'n_applic_infq']) 
+leftjoin2 = create_dataset_join2(leftjoin1 , maag_repa , ['N_APPLIC_INFQ' ,'C_MAST_AGREM_REFER'])
+leftjoin3 = create_dataset_join3(leftjoin2 , maag_raty , 'c_mast_agrem_refer')
 
 save_parquet(leftjoin1, AppConfig.path1)
 save_parquet(leftjoin2, AppConfig.path2)
