2b2a31961f72b1d6db9a0f0ede5d77d295dc0521
HafsiAhmed1920 <126480771+HafsiAhmed1920@users.noreply.github.com>
2023-08-22 17:53:50 +0200

    Add files via upload

--- process/process.py
+++ process/process.py
@@ -5,14 +5,41 @@ def left_join(df1: DataFrame, df2: DataFrame, column_name: str):
     result = df1.join(df2, on=column_name, how='left')
     return result
 
-def right_join(df1: DataFrame, df2: DataFrame, column_name: str):
-    result = df1.join(df2, on=column_name, how='right')
-    return result
+def create_dataset_join1(
+    maag_master: DataFrame,
+    reac_ref: DataFrame,
+    join_columns: list
+) -> DataFrame:
+    """
+    This function creates a dataset by performing a left join operation between two dataframes using the specified join columns.
+   
+    """
+    result_df = maag_master.join(reac_ref, on=join_columns, how='left')
+    return result_df
 
-def inner_join(df1: DataFrame, df2: DataFrame, column_name: str):
-    result = df1.join(df2, on=column_name, how='inner')
-    return result
 
-def outer_join(df1: DataFrame, df2: DataFrame, column_name: str):
-    result = df1.join(df2, on=column_name, how='outer')
-    return result
+def create_dataset_join2(
+    leftjoin1: DataFrame,
+    maag_repa: DataFrame,
+    join_columns: list
+) -> DataFrame:
+    """
+    This function creates a dataset by performing a left join operation between two dataframes using the specified join columns.
+    
+    """
+    result_df = leftjoin1.join(maag_repa, on=join_columns, how='left')
+    return result_df
+
+
+def create_dataset_join3(
+    leftjoin2: DataFrame,
+    maag_raty: DataFrame,
+    join_column: str
+) -> DataFrame:
+    """
+    This function creates a dataset by performing a left join operation between two dataframes using the specified join column.
+    
+    """
+    result_df = leftjoin2.join(maag_raty, on=join_column, how='left')
+    return result_df
+
