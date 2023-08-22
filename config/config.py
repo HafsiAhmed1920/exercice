9f276942a56b6f6026d744720aed9c591a257cd8
HafsiAhmed1920 <126480771+HafsiAhmed1920@users.noreply.github.com>
2023-08-22 17:37:42 +0200

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -17,7 +17,7 @@ class AppConfig:
 
 
 
-    app_config = Config(
+    app_config = AppConfig(
     maag_repa_path = "/FileStore/tables/tables (1)/maag_repa_rrol_linked/maag_repa_rrol_linked"
     maag_master_path = "/FileStore/tables/tables (1)/tables/maag_master_agrem"
     reac_ref_path = "/FileStore/tables/tables (1)/tables/reac_ref_act_type"
