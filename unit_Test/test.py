3a1ab1d7b53530ce2264a8a04167c7d25b9d6d5a
Ahmed Hafsi <ahafsi@dom.tti>
2023-08-25 15:27:49 +0200

    bugs fixed

--- unit_Test/test.py
+++ unit_Test/test.py
@@ -1,121 +1,62 @@
-<<<<<<< HEAD
-import unittest
+import pytest
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType, StructField, StringType
 
 
 def create_dataset_join3(
-    spark : SparkSession, 
+    spark: SparkSession,
     leftjoin2: DataFrame,
     maag_raty: DataFrame,
     join_column: str,
-    schema: StructType
+    schema: StructType,
 ) -> DataFrame:
     """
     Creates a dataset by performing a left join operation.
     """
     result_df = (
-        leftjoin2.join(maag_raty, on=join_column, how='left')
+        leftjoin2.join(maag_raty, on=join_column, how="left")
         .distinct()
         .select(*schema.fieldNames())
     )
     return result_df
 
 
-class TestCreateDatasetJoin3(unittest.TestCase):
-    # Define a class method that is run once before all tests in the test case
-    @classmethod
-    def setUpClass(cls):
-        # Create a SparkSession object
-        cls.spark = SparkSession.builder.appName("TestCreateDatasetJoin3").getOrCreate()
-        # Create a DataFrame to represent the leftjoin2 input data
-        cls.leftjoin2 = cls.spark.createDataFrame(
-            [("A", 1), ("B", 2), ("C", 3)], ["id", "value"]
-        )
-        # Create a DataFrame to represent the maag_raty input data
-        cls.maag_raty = cls.spark.createDataFrame(
-            [("A", "x"), ("B", "y")], ["id", "name"]
-        )
-        # Define the join column for the join operation
-        cls.join_column = "id"
-        # Define the schema for the resulting DataFrame
-        cls.schema = StructType(
-            [
-                StructField("id", StringType(), True),
-                StructField("value", StringType(), True),
-                StructField("name", StringType(), True),
-            ]
-        )
+@pytest.fixture(scope="module")
+def spark():
+    # Create a SparkSession object
+    spark = SparkSession.builder.appName("TestCreateDatasetJoin3").master("local[*]").getOrCreate()
+    yield spark
+    # Stop the SparkSession object to release resources
+    spark.stop()
 
-    # Define a test method that tests the create_dataset_join3 function
-    def test_create_dataset_join3(self):
-        # Call the create_dataset_join3 function with the test data and store the result
-        result_df = create_dataset_join3(
-            self.spark, self.leftjoin2, self.maag_raty, self.join_column, self.schema
-        )
-        # Create an expected DataFrame that represents the expected result of the join operation
-        expected_df = self.spark.createDataFrame(
-            [("A", 1, "x"), ("B", 2, "y"), ("C", 3, None)],
-            ["id", "value", "name"],
-        )
-        # Check if the resulting DataFrame is equal to the expected DataFrame
-        self.assertTrue(result_df.subtract(expected_df).count() == 0)
-        self.assertTrue(expected_df.subtract(result_df).count() == 0)
 
-    # Define a class method that is run once after all tests in the test case have completed
-    @classmethod
-    def tearDownClass(cls):
-        # Stop the SparkSession object to release resources
-        cls.spark.stop()
-=======
-import unittest
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import StructType, StructField, StringType
+@pytest.fixture(scope="module")
+def test_data(spark):
+    # Create a DataFrame to represent the leftjoin2 input data
+    leftjoin2 = spark.createDataFrame([("A", 1), ("B", 2), ("C", 3)], ["id", "value"])
+    # Create a DataFrame to represent the maag_raty input data
+    maag_raty = spark.createDataFrame([("A", "x"), ("B", "y")], ["id", "name"])
+    # Define the join column for the join operation
+    join_column = "id"
+    # Define the schema for the resulting DataFrame
+    schema = StructType(
+        [
+            StructField("id", StringType(), True),
+            StructField("value", StringType(), True),
+            StructField("name", StringType(), True),
+        ]
+    )
+    return leftjoin2, maag_raty, join_column, schema
 
-# Define a test case class that inherits from unittest.TestCase and SparkSession
-class TestCreateDatasetJoin3(unittest.TestCase):
-    # Define a class method that is run once before all tests in the test case
-    @classmethod
-    def setUpClass(cls):
-        # Create a SparkSession object
-        cls.spark = SparkSession.builder.appName("TestCreateDatasetJoin3").getOrCreate()
-        # Create a DataFrame to represent the leftjoin2 input data
-        cls.leftjoin2 = cls.spark.createDataFrame(
-            [("A", 1), ("B", 2), ("C", 3)], ["id", "value"]
-        )
-        # Create a DataFrame to represent the maag_raty input data
-        cls.maag_raty = cls.spark.createDataFrame(
-            [("A", "x"), ("B", "y")], ["id", "name"]
-        )
-        # Define the join column for the join operation
-        cls.join_column = "id"
-        # Define the schema for the resulting DataFrame
-        cls.schema = StructType(
-            [
-                StructField("id", StringType(), True),
-                StructField("value", StringType(), True),
-                StructField("name", StringType(), True),
-            ]
-        )
 
-    # Define a test method that tests the create_dataset_join3 function
-    def test_create_dataset_join3(self):
-        # Call the create_dataset_join3 function with the test data and store the result
-        result_df = create_dataset_join3(
-            self.spark, self.leftjoin2, self.maag_raty, self.join_column, self.schema
-        )
-        # Create an expected DataFrame that represents the expected result of the join operation
-        expected_df = self.spark.createDataFrame(
-            [("A", 1, "x"), ("B", 2, "y"), ("C", 3, None)],
-            ["id", "value", "name"],
-        )
-        # Check if the resulting DataFrame is equal to the expected DataFrame
-        self.assertTrue(result_df.subtract(expected_df).count() == 0)
-        self.assertTrue(expected_df.subtract(result_df).count() == 0)
-
-    # Define a class method that is run once after all tests in the test case have completed
-    @classmethod
-    def tearDownClass(cls):
-        # Stop the SparkSession object to release resources
-        cls.spark.stop()
->>>>>>> 3b82bc29147ae04bb8ae3be6c84f134bf5c80c1d
+def test_create_dataset_join3(spark, test_data):
+    leftjoin2, maag_raty, join_column, schema = test_data
+    # Call the create_dataset_join3 function with the test data and store the result
+    result_df = create_dataset_join3(spark, leftjoin2, maag_raty, join_column, schema)
+    # Create an expected DataFrame that represents the expected result of the join operation
+    expected_df = spark.createDataFrame(
+        [("A", 1, "x"), ("B", 2, "y"), ("C", 3, None)], ["id", "value", "name"]
+    )
+    # Check if the resulting DataFrame is equal to the expected DataFrame
+    assert result_df.subtract(expected_df).count() == 0
+    assert expected_df.subtract(result_df).count() == 0
