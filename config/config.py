4322eefbb3eab28182676a92f71b81753beb0137
Ahmed Hafsi <ahafsi@dom.tti>
2023-08-25 15:29:48 +0200

    updates comming

--- config/config.py
+++ config/config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from dataclasses import dataclass
 
 @dataclass
@@ -22,33 +21,3 @@ app_config = AppConfig(
     path2=r"C:\Users\ahafsi\Downloads\tables (1)\saved\join2",
     path3=r"C:\Users\ahafsi\Downloads\tables (1)\saved\join3"
 )
-=======
-# Config
-from dataclasses import dataclass
-
-@dataclass
-class AppConfig:
-   
-    maag_repa_path : str
-    maag_master_path : str
-    reac_ref_path : str
-    rtpa_ref_path : str
-    maag_raty_path : str
-    path1 : str
-    path2  : str
-    path3  : str 
-
-
-
-
-
-    app_config = AppConfig(
-    maag_repa_path = "/FileStore/tables/tables (1)/maag_repa_rrol_linked/maag_repa_rrol_linked"
-    maag_master_path = "/FileStore/tables/tables (1)/tables/maag_master_agrem"
-    reac_ref_path = "/FileStore/tables/tables (1)/tables/reac_ref_act_type"
-    rtpa_ref_path = "/FileStore/tables/tables (1)/tables/rtpa_ref_third_party"
-    maag_raty_path = "/FileStore/tables/tables (1)/tables/maag_raty_linked"
-    path1  = " paath here "
-    path2  = "path/to/save"
-    path3  = "path/to/save" )
->>>>>>> 3b82bc29147ae04bb8ae3be6c84f134bf5c80c1d
--- context/context.py
+++ context/context.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 from pyspark.sql import SparkSession
-=======
-from pyspark.sql import SparkSession
->>>>>>> 3b82bc29147ae04bb8ae3be6c84f134bf5c80c1d
 spark = SparkSession.builder.appName("hello").getOrCreate()
